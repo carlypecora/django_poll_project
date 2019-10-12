fd5dc3c41e4d167f0dc4cf8332d5ce9da885fd8c
Carly Pecora <carly@carlypecora.com>
2019-10-12 19:20:02 -0400

    testing for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,5 @@
 import datetime
+import pdb
 
 from django.test import TestCase
 from django.utils import timezone
@@ -90,5 +91,24 @@ class QuestionIndexViewTests(TestCase):
 		response = self.client.get(reverse('polls:index'))
 		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 2.>', '<Question: Past question 1.>'])
 
+class QuestionDetailViewTests(TestCase):
+	def test_future_question(self):
+		"""
+		The detail view of a question with a pub_date in the future
+		returns a 404 not found.
+		"""
+		future_question = create_question(question_text='Future question.', days=5)
+		url = reverse('polls:detail', args=(future_question.id,))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 404)
+
+	def test_past_question(self):
+		"""
+		The details view of a question with a pub_date in the past displays the question's text
+		"""
+		past_question = create_question(question_text='Past question.', days=-5)
+		url = reverse('polls:detail', args=(past_question.id,))
+		response = self.client.get(url)
+		self.assertContains(response, past_question.question_text)
 
 # Create your tests here.
